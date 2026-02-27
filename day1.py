50dfd6adc84f59531a5f4ea77f59e7835f4a216c
navedatgames <navedatgames@gmail.com>
2026-02-27 16:53:42 +0530

    day1: learning data types

---
+++ day1.py
@@ -0,0 +1,24 @@
+name="naved"
+age = 28
+salary = 152.65
+is_valid  = True
+
+print(name)
+print(age)
+print(salary)
+print(is_valid)
+
+#get type
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_valid))
+
+#get user input type string
+name = input("Enter name")
+
+#type int
+age = int(input("ENter age"))
+
+print(name)
+print(age)
